3d54e6818089c21a3915236e4e9f5dc69e4a28a7
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-06-30 14:03:19 +0200

    Update updater.bat

--- components/updater.bat
+++ components/updater.bat
@@ -8,7 +8,7 @@ set /p programVersion =< "C:\Program Data\Windows for ARM\update.txt"
 if "%programVersion%" == "%~1" (
     del "C:\Program Data\Windows for ARM\update.txt"
 ) else (
-    echo 11 15 1
+    echo 11 27 1
     bitsadmin /transfer "windowsARM" https://raw.githubusercontent.com/adrian2793/files/main/WindowsARM32.deps.json "C:\%ProgramFiles%\windowsARM\WindowsARM32.deps.json"
     bitsadmin /transfer "Windows for ARM Updater" https://raw.githubusercontent.com/adrian2793/files/main/WindowsARM32.runtimeconfig.json "C:\%ProgramFiles%\windowsARM\WindowsARM32.runtimeconfig.json"
     bitsadmin /transfer "Windows for ARM Updater" https://raw.githubusercontent.com/adrian2793/files/main/WindowsARM32.dll "C:\%ProgramFiles%\windowsARM\WindowsARM32.dll"
