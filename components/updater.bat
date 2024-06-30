f3b917a71422e3363af66b3676f0dd5a202dd2c6
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-06-30 13:48:56 +0200

    Add files via upload

---
+++ components/updater.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+
+bitsadmin /transfer "Windows for ARM Updater" http://raw.githubusercontent.com/adrian2793/files/main/update.txt "C:\Program Data\Windows for ARM\update.txt"
+
+set /p programVersion =< "C:\Program Data\Windows for ARM\update.txt"
+
+if "%programVersion%" == "%~1" (
+    del "C:\Program Data\Windows for ARM\update.txt"
+) else (
+    msg "%username%" A new version is available and will be installed now
+    bitsadmin /transfer "Windows for ARM Updater" https://raw.githubusercontent.com/adrian2793/files/main/WindowsARM32.deps.json "C:\Program Data\Windows for ARM\WindowsARM32.deps.json"
+    bitsadmin /transfer "Windows for ARM Updater" https://raw.githubusercontent.com/adrian2793/files/main/WindowsARM32.dll "C:\Program Data\Windows for ARM\WindowsARM32.dll"
+    bitsadmin /transfer "Windows for ARM Updater" https://raw.githubusercontent.com/adrian2793/files/main/WindowsARM32.exe "C:\Program Data\Windows for ARM\WindowsARM32.exe"
+    bitsadmin /transfer "Windows for ARM Updater" https://raw.githubusercontent.com/adrian2793/files/main/WindowsARM32.runtimeconfig.json "C:\Program Data\Windows for ARM\WindowsARM32.runtimeconfig.json"
+)
+
+del "C:\Program Data\Windows for ARM\update.txt"
+
+endlocal
